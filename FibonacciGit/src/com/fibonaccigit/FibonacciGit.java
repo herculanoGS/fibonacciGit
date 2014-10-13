457485b21da3902acfcfbbd9c1d6ee900ed5b7a1
Herculano <herculano.san@gmail.com>
2014-10-13 13:25:19 -0300

    FiboGit

---
+++ FibonacciGit/src/com/fibonaccigit/FibonacciGit.java
@@ -0,0 +1,22 @@
+package com.fibonaccigit;
+
+public class FibonacciGit {
+	public static long fibo (int n){
+		long a = 0;
+		if(n< 2){
+		 a=n;	
+		}
+		else
+		 a= fibo(n - 1) + fibo(n - 2);	 
+		return a;
+		
+	}public static void main(String[] args) {
+		 // teste do programa. Imprime os 30 primeiros termos
+	       for (int i = 0; i <= 30; i++) {
+	           System.out.print("F(" + i + "):" + FibonacciGit.fibo(i) + "\t");
+
+	       }
+
+		}
+
+}
