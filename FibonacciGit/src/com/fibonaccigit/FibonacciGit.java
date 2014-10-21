f88ac5580ef64f21b2dda79aaeb1ee9c1f5302da
Keylla <keylla03@gmail.com>
2014-10-21 17:02:07 -0300

    teste git

--- FibonacciGit/src/com/fibonaccigit/FibonacciGit.java
+++ FibonacciGit/src/com/fibonaccigit/FibonacciGit.java
@@ -13,6 +13,7 @@ public class FibonacciGit {
 	}public static void main(String[] args) {
 
 	       for (int i = 0; i <= 30; i++) {
+	       	 if(i>=0)
 			 System.out.print("F(" + i + "):" + FibonacciGit.fibo(i) + "\t");
 			 
 	       }	 
